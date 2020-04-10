effceb060a3d91b2658ed256d9b3c6fb81b9871c
Dmitry Klionsky <dm.klionsky@gmail.com>
2020-04-10 11:03:08 +0300

    Refine script

--- roles/work/files/jenkins-build.sh
+++ roles/work/files/jenkins-build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 
-# The below vars are expected to be set
+# The below vars MUST be set
 # JENKINS_URL
 # JENKINS_FOLDER
 # JENKINS_USERNAME
 # JENKINS_API_TOKEN
+
+# The below var CAN be set
+# DEBUG=1
 
 if [[ ! $(git branch 2> /dev/null) ]]; then
     echo "Not a git repository"
@@ -16,10 +19,23 @@ if [[ ! -f Jenkinsfile ]]; then
     exit 1
 fi
 
-REPO=$(git remote get-url origin | sed -E 's#(.*):(.*)/(.*).git#\3#')
+REPO=$(git remote get-url origin | sed -E 's#(.*):(.*)/(.*)(\.git)+#\3#')
 BRANCH=$(git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+if [[ ${DEBUG} -ge 1 ]]; then
+    echo "REPO   : ${REPO}"
+    echo "BRANCH : ${BRANCH}"
+fi
 
-OUTPUT=$(curl --silent -XPOST --user ${JENKINS_USERNAME?}:${JENKINS_API_TOKEN?} ${JENKINS_URL?}/job/${JENKINS_FOLDER?}/job/$REPO/job/$BRANCH/build)
+COMMAND="curl --silent -XPOST --user ${JENKINS_USERNAME?}:${JENKINS_API_TOKEN?} ${JENKINS_URL?}/job/${JENKINS_FOLDER?}/job/$REPO/job/$BRANCH/build"
+if [[ ${DEBUG} -ge 1 ]]; then
+    echo "COMMAND: ${COMMAND}"
+fi
+
+OUTPUT=$(${COMMAND})
+if [[ ${DEBUG} -ge 1 ]]; then
+    echo "OUTPUT : ${OUTPUT}"
+fi
+
 if [[ ! $(echo ${OUTPUT} | grep -i "error") ]]; then
     echo "Build scheduled"
 else
