5c1ce1fcad43534d4b41919d710f5da811669898
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-02-15 15:01:29 +0300

    Improve script

--- roles/work/files/jenkins-build.sh
+++ roles/work/files/jenkins-build.sh
@@ -21,12 +21,14 @@ fi
 
 REPO=$(git remote get-url origin | sed -E -e 's#\.git$##' -e 's#(.*):(.*)/(.*)#\3#')
 BRANCH=$(git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+JOB_URL=${JENKINS_URL?}/job/${JENKINS_FOLDER?}/job/${REPO}/job/${BRANCH}
 if [[ ${DEBUG} -ge 1 ]]; then
     echo "REPO   : ${REPO}"
     echo "BRANCH : ${BRANCH}"
+    echo "JOB URL: ${JOB_URL}"
 fi
 
-COMMAND="curl --silent -XPOST --user ${JENKINS_USERNAME?}:${JENKINS_API_TOKEN?} ${JENKINS_URL?}/job/${JENKINS_FOLDER?}/job/$REPO/job/$BRANCH/build"
+COMMAND="curl --silent --include -XPOST --user ${JENKINS_USERNAME?}:${JENKINS_API_TOKEN?} ${JOB_URL}/build"
 if [[ ${DEBUG} -ge 1 ]]; then
     echo "COMMAND: ${COMMAND}"
 fi
@@ -36,9 +38,11 @@ if [[ ${DEBUG} -ge 1 ]]; then
     echo "OUTPUT : ${OUTPUT}"
 fi
 
-if [[ ! $(echo ${OUTPUT} | grep -i "error") ]]; then
-    echo "Build scheduled"
+if [[ $(echo ${OUTPUT} | grep -i "201 Created") ]]; then
+    echo "Job scheduled"
+    curl --silent ${JOB_URL}/api/json | jq -r .lastBuild.url
 else
+    echo "Job scheduling error"
     echo ${OUTPUT}
     exit 1
 fi
